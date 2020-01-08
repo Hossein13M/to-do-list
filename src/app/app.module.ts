f76a015867b410444990ed044c5dcf46868e0cc6
hossein13m <hossein13mousavi@gmail.com>
2020-01-08 18:17:59 +0330

    task component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,17 @@ import { NgModule } from '@angular/core';
 // angular material components are the bellow
 import { MatSliderModule } from '@angular/material/slider';
 import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {  
+  MatMenuModule,
+  MatCheckboxModule,
+  MatIconModule,  
+  MatNativeDateModule
+} from '@angular/material';
+
 
 
 // these are my own components and pages
@@ -15,6 +26,8 @@ import { PageCardComponent } from './components/page-card/page-card.component';
 import { MainPageComponent } from './pages/main-page/main-page.component';
 import { ListsPageComponent } from './pages/lists-page/lists-page.component';
 import { ListNameComponent } from './components/list-name/list-name.component';
+import { TaskComponent } from './components/task/task.component';
+
 
 @NgModule({
   declarations: [    
@@ -24,16 +37,25 @@ import { ListNameComponent } from './components/list-name/list-name.component';
     PageCardComponent,
     MainPageComponent,
     ListsPageComponent,
-    ListNameComponent
+    ListNameComponent,
+    TaskComponent,
+    
   ],
   imports: [
     MatSliderModule,
+    MatInputModule,    
+    MatSelectModule,
     MatButtonModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatNativeDateModule
+    
   ],
-  providers: [],
+  providers: [MatDatepickerModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
