dc6f760d2a9c373e263ffbb5ce29320f365bd675
hossein13m <hossein13mousavi@gmail.com>
2020-01-09 05:21:43 +0330

    combined two components, media queries added

--- src/app/components/list-item/list-item.component.ts
+++ src/app/components/list-item/list-item.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
+import { Identifiers } from '@angular/compiler';
+import { httpFactory } from '@angular/http/src/http_module';
+import { ListNameComponent } from '../list-name/list-name.component';
 
 
 @Component({
@@ -9,14 +12,35 @@ import { Http } from '@angular/http';
 })
 export class ListItemComponent implements OnInit {
 
+
+  
+
+  onCreateList:{
+    _id: number;
+    title: string;
+    isMain: boolean;
+    date:Date;
+  };
+
   lists:any[];
 
-  constructor(http: Http) { 
+  constructor(private http: Http) { 
     http.get('http://localhost:4000/api/lists').subscribe(response => {
-    console.log(response.json())  
-    this.lists = response.json()
+      console.log(response.json())  
+      this.lists = response.json()
     })
   }
+
+
+
+  updateList(onCreateList){
+    this.http.put('http://localhost:4000/api/lists' + '/' + (Math.random() * (10 - 5) + 5), {onCreateList})
+
+  }
+
+  
+
+  
 
   ngOnInit() {
   }
