8e87ba8185dbd50a4492bf769cfc0b260e9d2a85
Hossein Mousavi <hossein13mousavi@gmail.com>
2022-02-12 20:28:17 +0330

    feat(model): implement interceptor for handling request url

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { Interceptor } from './interceptor';
 
 @NgModule({
     declarations: [AppComponent],
     imports: [BrowserModule, AppRoutingModule],
-    providers: [],
+    providers: [
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: Interceptor,
+            multi: true,
+        },
+    ],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
