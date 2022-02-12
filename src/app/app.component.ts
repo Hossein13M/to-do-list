92129d64b0ec72af990bce9c24eac4cc563b6256
Hossein Mousavi <hossein13mousavi@gmail.com>
2022-02-12 19:54:32 +0330

    fix(global): prettify whole project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'to-do-list';
+    title = 'to-do-list';
 }
