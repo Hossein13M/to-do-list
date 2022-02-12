155e03e0c1cd9203e2f5d64fbe8df05f748605ab
Hossein Mousavi <hossein13mousavi@gmail.com>
2022-02-12 21:57:46 +0330

    fix(appComponentSpec)!: add appComponentSpec test type

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { AppModule } from './app.module';
@@ -12,7 +12,7 @@ describe('AppComponent', () => {
     });
 
     it('should create the app', () => {
-        const fixture = TestBed.createComponent(AppComponent);
+        const fixture: ComponentFixture<AppComponent> = TestBed.createComponent(AppComponent);
         const app = fixture.componentInstance;
         expect(app).toBeTruthy();
     });
