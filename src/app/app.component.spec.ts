ae7648fe22b2e5596e2800bae84666547f40256e
Hossein Mousavi <hossein13mousavi@gmail.com>
2022-02-12 21:26:31 +0330

    feat(global)!: implement initial test cases for appComponent and appService

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { AppModule } from './app.module';
 
 describe('AppComponent', () => {
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [RouterTestingModule],
+            imports: [RouterTestingModule, AppModule],
             declarations: [AppComponent],
         }).compileComponents();
     });
@@ -14,18 +15,5 @@ describe('AppComponent', () => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.componentInstance;
         expect(app).toBeTruthy();
-    });
-
-    it(`should have as title 'to-do-list'`, () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.componentInstance;
-        expect(app.title).toEqual('to-do-list');
-    });
-
-    it('should render title', () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        fixture.detectChanges();
-        const compiled = fixture.nativeElement as HTMLElement;
-        expect(compiled.querySelector('.content span')?.textContent).toContain('to-do-list app is running!');
     });
 });
--- src/app/services/app.service.ts
+++ src/app/services/app.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { List } from '../models/list.model';
 
-@Injectable()
+@Injectable({ providedIn: 'root' })
 export class AppService {
     constructor(private readonly http: HttpClient) {}
 
