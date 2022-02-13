748aa33be46a56a296916c1e385984674dd6b927
Hossein Mousavi <hossein13mousavi@gmail.com>
2022-02-13 23:46:05 +0330

    feat(lists): implement lists page

--- src/app/modules/lists/lists.module.ts
+++ src/app/modules/lists/lists.module.ts
@@ -4,9 +4,12 @@ import { ListsComponent } from './lists.component';
 import { ListsRoutingModule } from './lists-routing.module';
 import { NameDialogModule } from '../../components/name-dialog/name-dialog.module';
 import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
     declarations: [ListsComponent],
-    imports: [CommonModule, ListsRoutingModule, NameDialogModule, MatTableModule],
+    imports: [CommonModule, ListsRoutingModule, NameDialogModule, MatTableModule, MatButtonModule, MatIconModule, MatTooltipModule],
 })
 export class ListsModule {}
