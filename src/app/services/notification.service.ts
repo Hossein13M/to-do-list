5aa03f5a1aff281d0a40e965fa66754a34ac9752
Hossein Mousavi <hossein13mousavi@gmail.com>
2022-02-13 23:24:17 +0330

    feat(notificationService): change snackbar alert direction

--- src/app/services/notification.service.ts
+++ src/app/services/notification.service.ts
@@ -8,14 +8,14 @@ export class NotificationService {
     constructor(private snackBar: MatSnackBar) {}
 
     public onError(message: string, hasAction?: string): void {
-        this.snackBar.open(message, hasAction, { panelClass: 'snack-error', duration: 3000, direction: 'rtl' });
+        this.snackBar.open(message, hasAction, { panelClass: 'snack-error', duration: 3000, direction: 'ltr' });
     }
 
     public onSuccess(message: string, hasAction?: string): void {
-        this.snackBar.open(message, hasAction, { panelClass: 'snack-success', duration: 3000, direction: 'rtl' });
+        this.snackBar.open(message, hasAction, { panelClass: 'snack-success', duration: 3000, direction: 'ltr' });
     }
 
     public onInfo(message: string, hasAction?: string): void {
-        this.snackBar.open(message, hasAction, { panelClass: 'snack-info', duration: 3000, direction: 'rtl' });
+        this.snackBar.open(message, hasAction, { panelClass: 'snack-info', duration: 3000, direction: 'ltr' });
     }
 }
