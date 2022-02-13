33d3089a302d0bc606733304a9f522f0adc5ea13
Hossein Mousavi <hossein13mousavi@gmail.com>
2022-02-13 22:34:39 +0330

    feat(lists): implement update and remove list API calls

--- src/app/modules/lists/lists.module.ts
+++ src/app/modules/lists/lists.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListsComponent } from './lists.component';
 import { ListsRoutingModule } from './lists-routing.module';
+import { NameDialogModule } from '../../components/name-dialog/name-dialog.module';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
     declarations: [ListsComponent],
-    imports: [CommonModule, ListsRoutingModule],
+    imports: [CommonModule, ListsRoutingModule, NameDialogModule, MatTableModule],
 })
 export class ListsModule {}
