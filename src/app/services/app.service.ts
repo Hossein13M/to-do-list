d6e7e4f3d46b44c97caafb3bfa3aff140de673e7
Hossein Mousavi <hossein13mousavi@gmail.com>
2022-02-15 01:11:52 +0330

    style(lists): change service args and function return types

--- src/app/services/app.service.ts
+++ src/app/services/app.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { List } from '../models/list.model';
-import { Task } from '../models/task.model';
 
 @Injectable()
 export class AppService {
@@ -12,11 +11,11 @@ export class AppService {
         return this.http.get<any>(`/tasks`);
     }
 
-    public addTask(task: Task): Observable<any> {
-        return this.http.post<any>(`/tasks`, task);
+    public addTask(taskTitle: { title: string; list: List }): Observable<any> {
+        return this.http.post<any>(`/tasks`, taskTitle);
     }
 
-    public getTaskByListId(listId: number): Observable<any> {
+    public getTaskByListId(listId: string): Observable<any> {
         return this.http.get<any>(`/tasks/query/${listId}`);
     }
 
@@ -24,7 +23,7 @@ export class AppService {
         return this.http.get<any>(`/tasks/${taskId}`);
     }
 
-    public removeTaskById(taskId: number): Observable<any> {
+    public removeTaskById(taskId: string): Observable<any> {
         return this.http.delete<any>(`/tasks/${taskId}`);
     }
 
@@ -45,8 +44,8 @@ export class AppService {
         return this.http.get<Array<List>>(`/lists`);
     }
 
-    public getListById(listId: number): Observable<any> {
-        return this.http.get<any>(`/lists/${listId}`);
+    public getListById(listId: string): Observable<List> {
+        return this.http.get<List>(`/lists/${listId}`);
     }
 
     public removeListById(listId: string): Observable<any> {
